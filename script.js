318507eabb158d061fa9e2c545f168a5833c0b26
molinajimenez <molinajimenez01@gmail.com>
2020-04-09 23:06:48 -0600

    file

---
+++ script.js
@@ -0,0 +1,38 @@
+// Initialize the application
+ec.init({
+    app_id: "my-super-app", // use your application client_id tenes esto de tu app?
+    autoloadedflag: true, 
+    autoheight: true
+  });
+  
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+ec.order.extraFields.nombre = {
+    'title': 'Nombre para la factura',
+    'textPlaceholder': 'N',
+    'type': 'text',
+    'tip': 'Nombre o Consumidor final',
+    'required': true,
+    'checkoutDisplaySection': 'shipping_address'
+};
+
+ec.order.extraFields.nit = {
+    'title': 'Número de NIT',
+    'textPlaceholder': 'Package sign',
+    'type': 'text',
+    'tip': 'Ingrese el número de NIT',
+    'required': false,
+    'checkoutDisplaySection': 'shipping_address'
+};
+
+ec.order.extraFields.direccion = {
+    'title': 'Dirección',
+    'textPlaceholder': 'Package sign',
+    'type': 'text',
+    'tip': 'Ingrese la dirección para la factura',
+    'required': false,
+    'checkoutDisplaySection': 'shipping_address'
+};
+
+ec.refreshConfig();
